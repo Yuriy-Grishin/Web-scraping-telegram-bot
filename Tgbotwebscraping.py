533c92af8c9e57214d94832f7578b4c070c33dea
YuriyGrishin <104274570+Yuriy-Grishin@users.noreply.github.com>
2025-12-08 12:53:08 +0300

    Create Tgbotwebscraping.py

---
+++ Tgbotwebscraping.py
@@ -0,0 +1,43 @@
+import telebot
+from telebot import types
+from bs4 import BeautifulSoup
+import requests
+import re
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import json
+
+BOT_TOKEN = 'BOT_TOKEN'
+CHAT_ID = 'CHAT_ID'
+
+def func():
+    url = 'https://www.banki.ru/news/lenta/'
+    page = requests.get(url)
+
+    filteredNews = []
+    allNews = []
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    allNews =soup.find_all('a',       class_='lf473447f text-weight-medium',          string=re.compile(r'вклад', re.IGNORECASE)) 
+
+    for data in allNews:
+        filteredNews.append(data.text)
+
+    res = [re.sub(r'/\n','', i) for i in filteredNews]
+    res = [re.sub(r'\t','', i) for i in filteredNews]
+    
+    return '  '.join(res)
+
+async def send_result(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result = func()
+    await context.bot.send_message(chat_id=CHAT_ID, text=result)
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("send", send_result))
+
+    print("Бот запущен...")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+  
+if __name__ == '__main__':
+    main()
